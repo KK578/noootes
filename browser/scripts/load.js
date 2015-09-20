c49dd3731012bd1009be1d9244859a7526eabdf2
Kevin Kwan <KK1241@googlemail.com>
2015-09-20 16:28:39 +0100

    Load initial dependencies

--- browser/scripts/load.js
+++ browser/scripts/load.js
@@ -5,8 +5,33 @@
 //  3) Load App dependencies
 //  4) Hide Splash Screen
 (function () {
+    var initialDependencies = [
+        'bower_components/font-roboto/roboto.html',
+        'bower_components/paper-material/paper-material.html',
+        'bower_components/paper-spinner/paper-spinner.html'
+    ];
+
+    function startSplashScreen() {
+        console.log('Initial dependencies loaded');
+    }
+
     function loadInitialDependencies() {
-        console.log('WebComponents is Ready');
+        var loadedDependencies = 0;
+
+        function loaded() {
+            if (++loadedDependencies >= initialDependencies.length) {
+                window.setTimeout(startSplashScreen, 0);
+            }
+        }
+
+        for (var i = 0; i < initialDependencies.length; i++) {
+            var elementImport = document.createElement('link');
+            elementImport.rel = 'import';
+            elementImport.href = initialDependencies[i];
+            elementImport.onload = loaded;
+
+            document.head.appendChild(elementImport);
+        }
     }
 
     function checkNativeWebComponents() {
