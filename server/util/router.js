639fb8130ccaceac403672ffd680ec52df9b47ff
Kevin Kwan <KK1241@googlemail.com>
2015-09-20 17:57:29 +0100

    Inject browser_sync_client script if server is running in development mode

--- server/util/router.js
+++ server/util/router.js
@@ -1,9 +1,29 @@
-﻿module.exports = function (server, environment) {
-    var path = require('path');
+﻿var path = require('path');
+var fs = require('fs');
+
+function injectBrowserSync(req, res) {
+    var index = path.join(__dirname, '/../public/index.html');
+    fs.readFile(index, { encoding: 'utf-8' }, function (err, data) {
+        if (err) {
+            throw err;
+        }
+
+        // Find last </body> tag, and prepend script to it.
+        var script = '<script src="scripts/browser_sync_client.js" async></script>';
+        var response = data.replace(/<\/body>(?![\s\S]*<\/body>)/i, script + '</body>');
+
+        res.send(response);
+    });
+}
+
+module.exports = function (server, environment) {
     var express = require('express');
 
     // Developer routes
     if (environment === 'developer') {
+        // Inject BrowserSync client side handler
+        server.get('/', injectBrowserSync);
+
         server.use('/bower', express.static(
             path.join(__dirname, '/../../browser/bower_components/'))
         );
