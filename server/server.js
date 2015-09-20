1d1f5c2cc71cff80f43bebf130d198bcbeeb9ce3
Kevin Kwan <KK1241@googlemail.com>
2015-09-20 01:16:00 +0100

    Add main setup for server

--- server/server.js
+++ server/server.js
@@ -1,5 +1,8 @@
 ﻿module.exports = function (environment) {
     var server = require('express');
 
+    var util = require('./util/util.js');
+    util.setup(server);
+
     return server;
 };
---
+++ server/util/setup.js
@@ -0,0 +1,12 @@
+﻿module.exports = function (server) {
+    var path = require('path');
+    var ejs = require('ejs');
+
+    // Setup renderer for HTML
+    server.set('views', path.join(__dirname, '/../public/'));
+    server.engine('html', ejs.renderFile);
+    server.set('view engine', 'html');
+
+    // GZIP
+    server.use(require('compression')());
+};
