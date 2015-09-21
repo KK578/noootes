9fb45d84bd368bde7d457915bf2a67b8299281a8
Kevin Kwan <KK1241@googlemail.com>
2015-09-21 15:14:32 +0100

    Add toast-message script

---
+++ browser/scripts/toast.js
@@ -0,0 +1,7 @@
+﻿window.addEventListener('toast-message', function (event) {
+    var detail = event.detail;
+
+    var toast = document.getElementById('noootes-toast');
+    toast.text = detail.message;
+    toast.show();
+});
