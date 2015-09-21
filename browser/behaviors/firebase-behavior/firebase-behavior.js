00196e0b5f36354943714da9cb3add9b5b6caa4a
Kevin Kwan <KK1241@googlemail.com>
2015-09-21 23:12:14 +0100

    Add firebase-behavior.

---
+++ browser/behaviors/firebase-behavior/firebase-behavior.js
@@ -0,0 +1,8 @@
+﻿Noootes.Behaviors.FirebaseBehavior = {
+    bindToLogin: function (callback) {
+        window.addEventListener('firebase-login-success', callback.bind(this));
+    },
+    bindToLogout: function (callback) {
+        window.addEventListener('firebase-logout-success', callback.bind(this));
+    }
+};
