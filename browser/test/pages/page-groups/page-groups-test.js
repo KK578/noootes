d47f6939b067a9aa88a3e63c59749a24fec3a7e7
Kevin Kwan <KK1241@googlemail.com>
2015-09-24 00:37:11 +0100

    Test suite for templating items from firebase.

--- browser/test/pages/page-groups/page-groups-test.js
+++ browser/test/pages/page-groups/page-groups-test.js
@@ -12,6 +12,19 @@ describe('<page-groups>', function () {
     });
 
     describe('Public Groups', function () {
-        it('has no tests written yet');
+        it('should set location for firebase-collection', function () {
+            var firebase = pageGroups.querySelector('#firebase-public-groups');
+            firebase.location.should.equal('https://noootes-staging.firebaseio.com/groups/public');
+        });
+
+        it('should template items', function (done) {
+            var container = pageGroups.querySelector('#public-groups');
+            var handle = window.setInterval(function () {
+                if (container.childNodes.length > 1) {
+                    window.clearInterval(handle);
+                    done();
+                }
+            }, 500);
+        });
     });
 });
