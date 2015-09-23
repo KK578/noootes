476f56ad218361208220aa3680ee24623457c004
Kevin Kwan <KK1241@googlemail.com>
2015-09-24 00:27:11 +0100

    Scaffold functionalities.

--- browser/test/pages/page-groups/page-groups-test.js
+++ browser/test/pages/page-groups/page-groups-test.js
@@ -1,5 +1,17 @@
 chai.should();
 
 describe('<page-groups>', function () {
-    it('has no tests written yet');
+    var pageGroups;
+
+    before(function () {
+        pageGroups = document.querySelector('page-groups');
+    });
+
+    describe('Search', function () {
+        it('has no tests written yet');
+    });
+
+    describe('Public Groups', function () {
+        it('has no tests written yet');
+    });
 });
