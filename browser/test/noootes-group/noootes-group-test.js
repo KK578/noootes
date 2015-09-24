9836742ebd368c71bff5b70703226e4fbe567f01
Kevin Kwan <KK1241@googlemail.com>
2015-09-24 16:22:13 +0100

    Add test for getting public data from firebase.

--- browser/test/noootes-group/noootes-group-test.js
+++ browser/test/noootes-group/noootes-group-test.js
@@ -1,5 +1,27 @@
 chai.should();
 
 describe('<noootes-group>', function () {
-    it('has no tests written yet');
+    var noootesGroup;
+
+    before(function () {
+        noootesGroup = document.querySelector('noootes-group');
+    });
+
+    it('should fetch data from firebase on changing group', function (done) {
+        noootesGroup.group = '-JzwgEvXhmCq6xVh7Wja';
+
+        var data;
+        var handle = window.setInterval(function () {
+            data = noootesGroup._data;
+
+            if (data) {
+                data.code.should.equal('WCT');
+                data.title.should.equal('Web Component Testing');
+                data.owner.should.equal('bdcdc03c-01b8-4080-b77c-40df9b64067b');
+
+                window.clearInterval(handle);
+                done();
+            }
+        }, 500);
+    });
 });
