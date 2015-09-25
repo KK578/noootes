4e6bba20d9d1cb34ee72e8af3451e1e190b3e3dd
Kevin Kwan <KK1241@googlemail.com>
2015-09-25 22:29:09 +0100

    Setup test suite for templating items from firebase.

--- browser/test/pages/page-personal/page-personal-test.js
+++ browser/test/pages/page-personal/page-personal-test.js
@@ -14,7 +14,38 @@ describe('<page-personal>', function () {
     });
 
     describe('Owned Groups', function () {
-        it('has no tests written yet.');
+        it('should set location for firebase-collection on clicking button', function () {
+            var firebase = pagePersonal.querySelector('#firebase-groups-owned');
+            (firebase.location === undefined).should.equal(true);
+
+            var button = pagePersonal.querySelector('#button-groups-owned');
+            button.click();
+            firebase.location.should.equal('https://noootes-staging.firebaseio.com/users/personal/3f8f4d76-8d58-4a56-a3f2-661dce66d085/owned');
+        });
+
+        it('should template items from firebase-collection', function (done) {
+            var container = pagePersonal.querySelector('#container-groups-owned');
+            var handle = window.setInterval(function () {
+                if (container.childNodes.length > 1) {
+                    window.clearInterval(handle);
+                    done();
+                }
+            }, 500);
+        });
+
+        it('should set group property on templated items', function () {
+            var container = pagePersonal.querySelector('#container-public-groups');
+            var children = container.childNodes;
+
+            for (var i = 0; i < children.length - 1; i++) {
+                if (i === children.length) {
+                    children[i].nodeName.should.equal('TEMPLATE');
+                }
+                else {
+                    children[i].group.should.match(/.{20}/);
+                }
+            }
+        });
     });
 
     describe('Joined Groups', function () {
