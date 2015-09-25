f00704bc6dae85099a40e0e0697d13eafd1edba0
Kevin Kwan <KK1241@googlemail.com>
2015-09-25 22:20:20 +0100

    Scaffold test suite.

--- browser/test/pages/page-personal/page-personal-test.js
+++ browser/test/pages/page-personal/page-personal-test.js
@@ -1,5 +1,23 @@
 chai.should();
 
 describe('<page-personal>', function () {
-    it('has no tests written yet');
+    var pagePersonal;
+
+    before(function (done) {
+        pagePersonal = document.querySelector('page-personal');
+
+        var firebase = Noootes.FirebaseRef();
+        firebase.authWithPassword({
+            email: 'Web@Component.Tester',
+            password: 'WebComponentTester'
+        }, done);
+    });
+
+    describe('Owned Groups', function () {
+        it('has no tests written yet.');
+    });
+
+    describe('Joined Groups', function () {
+        it('has no tests written yet.');
+    });
 });
