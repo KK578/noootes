c17437be81bea391fd6e09df61be3878008f49fa
Kevin Kwan <KK1241@googlemail.com>
2015-09-26 00:20:03 +0100

    Use Sinon to not spam firebase.

--- browser/test/noootes-app/noootes-app-test.js
+++ browser/test/noootes-app/noootes-app-test.js
@@ -1,23 +1,14 @@
+/* globals sinon */
+/* jshint -W030 */
 chai.should();
 
 describe('<noootes-app>', function () {
-    function listenToResponseOnFiringEvent(firedEvent, responseEvent, eventDetail, done, assertions) {
-        function listener(event) {
-            if (assertions) {
-                assertions(event);
-            }
-
-            window.removeEventListener(responseEvent, listener);
-            done();
-        }
-
-        window.addEventListener(responseEvent, listener);
-        window.dispatchEvent(new CustomEvent(firedEvent, { detail: eventDetail }));
-    }
-
     var testUser;
+    var firebase;
 
     before(function () {
+        firebase = document.querySelector('#firebase-auth');
+
         var name = 'wct-tester-' + Math.round(50000 * Math.random());
         var email = name + '@test.suite';
         var password = 'wct-tester';
@@ -29,228 +20,53 @@ describe('<noootes-app>', function () {
         };
     });
 
-    after(function (done) {
-        var firebase = document.querySelector('#firebase-auth');
-
-        firebase.addEventListener('user-removed', function () {
-            done();
+    it('should call firebase-auth#login on "firebase-login"', function () {
+        sinon.stub(firebase, 'login', function () {
+            firebase.fire('login');
         });
 
-        firebase.addEventListener('error', function (event) {
-            console.log(new Error('Failed to remove ' + testUser.email));
-            console.log(event);
-            done();
+        var detail = {
+            email: testUser.email,
+            password: testUser.password
+        };
+
+        window.dispatchEvent(new CustomEvent('firebase-login', { detail: detail }));
+        firebase.login.should.have.been.calledWith(detail);
+        firebase.login.restore();
+    });
+
+    it('should call firebase-auth#createUser on "firebase-register"', function () {
+        sinon.stub(firebase, 'createUser', function () {
+            firebase.fire('user-created');
         });
 
-        firebase.removeUser(testUser.newEmail, testUser.newPassword);
-    });
-
-    /////////////////////////////////////////////
-    // Initial Login Test
-    /////////////////////////////////////////////
-    it('should fire "firebase-login-error" on "firebase-login" with unregistered email', function (done) {
-        function assertions(event) {
-            event.detail.code.should.equal('INVALID_USER');
-            event.detail.message.should.equal('The specified user does not exist.');
-        }
-
         var detail = {
             email: testUser.email,
             password: testUser.password
         };
 
-        listenToResponseOnFiringEvent('firebase-login', 'firebase-login-error', detail, done, assertions);
+        window.dispatchEvent(new CustomEvent('firebase-register', { detail: detail }));
+        firebase.createUser.should.have.been.calledWith(detail.email, detail.password);
+        firebase.createUser.restore();
     });
 
-    /////////////////////////////////////////////
-    // Registration Tests
-    /////////////////////////////////////////////
-    it('should register new user and fire "toast-message" on "firebase-register"', function (done) {
-        var counter = 0;
-
-        function end() {
-            if (++counter === 2) {
-                done();
-            }
-        }
-
-        function toastAssertions(event) {
-            event.detail.message.should.equal('Successfully registered your account!\nPlease log in.');
-
-            window.removeEventListener('toast-message', toastAssertions);
-            end();
-        }
-
-        var detail = {
-            email: testUser.email,
-            password: testUser.password
-        };
-
-        window.addEventListener('toast-message', toastAssertions);
-        listenToResponseOnFiringEvent('firebase-register', 'firebase-register-success', detail, end);
-    });
-
-    it('should fire "firebase-register-error" on "firebase-register" with the same email', function (done) {
-        function assertions(event) {
-            event.detail.code.should.equal('EMAIL_TAKEN');
-            event.detail.message.should.equal('The specified email address is already in use.');
-        }
-
-        var detail = {
-            email: testUser.email,
-            password: testUser.password
-        };
-
-        listenToResponseOnFiringEvent('firebase-register', 'firebase-register-error', detail, done, assertions);
-    });
-
-    /////////////////////////////////////////////
-    // Login Tests
-    /////////////////////////////////////////////
-    it('should fire "firebase-login-error" on "firebase-login" with incorrect password ', function (done) {
-        function assertions(event) {
-            event.detail.code.should.equal('INVALID_PASSWORD');
-            event.detail.message.should.equal('The specified password is incorrect.');
-        }
-
-        var detail = {
-            email: testUser.email,
-            password: 'ThisIsn\'tThePassword'
-        };
-
-        listenToResponseOnFiringEvent('firebase-login', 'firebase-login-error', detail, done, assertions);
-    });
-
-    it('should fire "firebase-login-success" on "firebase-login" with the correct user details', function (done) {
-        function assertions(event) {
-            event.detail.user.password.email.should.equal(testUser.email);
-
+    it('should change page on "firebase-login-success"', function (done) {
+        window.addEventListener('firebase-login-success', function () {
             var element = document.querySelector('noootes-app');
             element._selectedPage.should.equal(1);
-        }
+            done();
+        });
 
-        var detail = {
-            email: testUser.email,
-            password: testUser.password
-        };
-
-        listenToResponseOnFiringEvent('firebase-login', 'firebase-login-success', detail, done, assertions);
+        window.dispatchEvent(new CustomEvent('firebase-login-success'));
     });
 
-    /////////////////////////////////////////////
-    // Logout Test
-    /////////////////////////////////////////////
-    it('should fire "firebase-logout-success" on "firebase-logout"', function (done) {
-        function assertions() {
-            var element = document.querySelector('noootes-app');
-            element._selectedPage.should.equal(0);
-        }
+    it('should call firebase-auth#logout on "firebase-logout"', function () {
+        sinon.stub(firebase, 'logout', function () {
+            firebase.fire('logout');
+        });
 
-        var detail = {};
-
-        listenToResponseOnFiringEvent('firebase-logout', 'firebase-logout-success', detail, done, assertions);
+        window.dispatchEvent(new CustomEvent('firebase-logout'));
+        firebase.logout.should.have.been.called;
+        firebase.logout.restore();
     });
-
-    /////////////////////////////////////////////
-    // Change Email Tests
-    /////////////////////////////////////////////
-    it('should fire "firebase-change-email-error" on "firebase-change-email" with invalid email', function (done) {
-        function assertions(event) {
-            event.detail.code.should.equal('INVALID_USER');
-            event.detail.message.should.equal('The specified user does not exist.');
-        }
-
-        var detail = {
-            email: 'not-' + testUser.email,
-            password: testUser.password,
-            newEmail: testUser.newEmail
-        };
-
-        listenToResponseOnFiringEvent('firebase-change-email', 'firebase-change-email-error', detail, done, assertions);
-    });
-
-    it('should fire "firebase-change-email-error" on "firebase-change-email" with invalid password', function (done) {
-        function assertions(event) {
-            event.detail.code.should.equal('INVALID_PASSWORD');
-            event.detail.message.should.equal('The specified password is incorrect.');
-        }
-
-        var detail = {
-            email: testUser.email,
-            password: 'not-' + testUser.password,
-            newEmail: testUser.newEmail
-        };
-
-        listenToResponseOnFiringEvent('firebase-change-email', 'firebase-change-email-error', detail, done, assertions);
-    });
-
-    it('should fire "firebase-change-email-error" on "firebase-change-email" with a new email that is already in use', function (done) {
-        function assertions(event) {
-            event.detail.code.should.equal('EMAIL_TAKEN');
-            event.detail.message.should.equal('The specified email address is already in use.');
-        }
-
-        var detail = {
-            email: testUser.email,
-            password: testUser.password,
-            newEmail: testUser.email
-        };
-
-        listenToResponseOnFiringEvent('firebase-change-email', 'firebase-change-email-error', detail, done, assertions);
-    });
-
-    it('should fire "firebase-change-email-success" on "firebase-change-email" with the correct user details', function (done) {
-        var detail = {
-            email: testUser.email,
-            password: testUser.password,
-            newEmail: testUser.newEmail
-        };
-
-        listenToResponseOnFiringEvent('firebase-change-email', 'firebase-change-email-success', detail, done);
-    });
-
-    /////////////////////////////////////////////
-    // Change Password Tests
-    /////////////////////////////////////////////
-    it(' should fire "firebase-change-password-error" on "firebase-change-password" with invalid user email', function (done) {
-        function assertions(event) {
-            event.detail.code.should.equal('INVALID_USER');
-            event.detail.message.should.equal('The specified user does not exist.');
-        }
-
-        var detail = {
-            email: 'not-' + testUser.newEmail,
-            password: testUser.password,
-            newPassword: testUser.newPassword
-        };
-
-        listenToResponseOnFiringEvent('firebase-change-password', 'firebase-change-password-error', detail, done, assertions);
-    });
-
-    it('should fire "firebase-change-password-error" on "firebase-change-password" with invalid password', function (done) {
-        function assertions(event) {
-            event.detail.code.should.equal('INVALID_PASSWORD');
-            event.detail.message.should.equal('The specified password is incorrect.');
-        }
-
-        var detail = {
-            email: testUser.newEmail,
-            password: 'not-' + testUser.password,
-            newPassword: testUser.newPassword
-        };
-
-        listenToResponseOnFiringEvent('firebase-change-password', 'firebase-change-password-error', detail, done, assertions);
-    });
-
-    it('should fire "firebase-change-password-success" on "firebase-change-password" with correct user details', function (done) {
-        var detail = {
-            email: testUser.newEmail,
-            password: testUser.password,
-            newPassword: testUser.newPassword
-        };
-
-        listenToResponseOnFiringEvent('firebase-change-password', 'firebase-change-password-success', detail, done);
-    });
-
-    it('could fire "firebase-delete-account-success" on "firebase-delete-account"');
 });
