e47041aa63127b84b94fba81003a96c2443964c1
Kevin Kwan <KK1241@googlemail.com>
2015-09-26 20:32:42 +0100

    Major group functions jsDocs.

--- browser/behaviors/group-behavior/group-behavior.js
+++ browser/behaviors/group-behavior/group-behavior.js
@@ -1,4 +1,21 @@
 ﻿Noootes.Behaviors.GroupBehavior = {
+    ///////////////////////
+    // Main Group Functions
+    ///////////////////////
+    /**
+     * Callback for #searchGroup.
+     *
+     * @callback searchGroupCallback
+     * @param {String|false} result - Returns group key if group was found, false otherwise.
+     */
+
+    /**
+     * Searches Firebase for a group located at uid/code.
+     *
+     * @param {String} uid - UID to query.
+     * @param {String} code - Code to query under UID.
+     * @param {searchGroupCallback} callback - Function to call on result.
+     */
     searchGroup: function (uid, code, callback) {
         var firebase = Noootes.FirebaseRef('groups/access/id');
 
@@ -8,29 +25,51 @@
         });
     },
 
+    /**
+     * Create a new group under the current logged in user.
+     *
+     * @param {Object} meta - Metadata for group to be created.
+     * @property {String} meta.code - New group's code.
+     * @property {String} meta.title - New group's title.
+     * @property {String} meta.description - New group's description.
+     * @param {Object} access - Access data for new group.
+     * @property {String} access.global - Set to 'none', 'read' or 'write'.
+     * @property {Any} access.public - Set any value to make group public.
+     */
     createGroup: function (meta, access) {
         var user = Noootes.Firebase.User;
 
         if (user) {
-            var firebase = Noootes.FirebaseRef();
-            var key = firebase.push().key();
+            this.searchGroup(user.uid, meta.code, function (exists) {
+                if (exists) {
+                    return;
+                }
 
-            // Add group to user's owned groups.
-            this.editUserOwned(key, true);
-            // Add public group metadata.
-            // Ensure that owner matches current user uid.
-            meta.owner = user.uid;
-            this.editGroupMetadata(key, meta);
-            // Add group identifier (User/Code)
-            this.editGroupAccessId(meta.code, key);
-            // Add group's global access permissions.
-            this.editGroupAccessGlobal(key, access.global);
+                var firebase = Noootes.FirebaseRef();
+                var key = firebase.push().key();
 
-            // Add group to public listings if requested.
-            this.editGroupVisibility(key, access.public);
+                // Add group to user's owned groups.
+                this.editUserOwned(key, true);
+                // Add public group metadata.
+                // Ensure that owner matches current user uid.
+                meta.owner = user.uid;
+                this.editGroupMetadata(key, meta);
+                // Add group identifier (User/Code)
+                this.editGroupAccessId(meta.code, key);
+                // Add group's global access permissions.
+                this.editGroupAccessGlobal(key, access.global);
+
+                // Add group to public listings if requested.
+                this.editGroupVisibility(key, access.public);
+            }.bind(this));
         }
     },
 
+    /**
+     * Delete group under the current logged in user.
+     *
+     * @param {String} code - Group code to delete.
+     */
     deleteGroup: function (code) {
         var user = Noootes.Firebase.User;
 
@@ -52,6 +91,9 @@
         }
     },
 
+    /////////////////////////
+    // Group Detail Functions
+    /////////////////////////
     editGroupMetadata: function (key, value) {
         var firebase = Noootes.FirebaseRef('groups/metadata').child(key);
         firebase.set(value);
@@ -70,10 +112,12 @@
 
     editGroupVisibility: function (key, value) {
         var firebase = Noootes.FirebaseRef('groups/public').child(key);
-        firebase.set(value ? value : null);
+        firebase.set(value ? true : null);
     },
 
-    // User Related
+    /////////////////
+    // User Functions
+    /////////////////
     editUserOwned: function (key, value) {
         var user = Noootes.Firebase.User;
         var firebase = Noootes.FirebaseRef('users/personal').child(user.uid).child('owned')
