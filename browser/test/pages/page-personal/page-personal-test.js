f56d5a0036e16f9d15d2d848b224fdf3ce58897d
Kevin Kwan <KK1241@googlemail.com>
2015-09-26 14:17:13 +0100

    Setup test suite for creating groups.

--- browser/test/pages/page-personal/page-personal-test.js
+++ browser/test/pages/page-personal/page-personal-test.js
@@ -1,6 +1,26 @@
 chai.should();
 
 describe('<page-personal>', function () {
+    function listenToEventOnClickingButton(form, eventName, inputs, button, done, assertions) {
+        function listener(event) {
+            if (assertions) {
+                assertions(event);
+            }
+
+            window.removeEventListener(eventName, listener);
+            done();
+        }
+
+        window.addEventListener(eventName, listener);
+
+        for (var i = 0; i < inputs.length; i++) {
+            var input = inputs[i];
+            form.querySelector(input.name).value = input.value;
+        }
+
+        form.querySelector(button).click();
+    }
+
     var pagePersonal;
 
     before(function (done) {
@@ -48,6 +68,78 @@ describe('<page-personal>', function () {
                     children[i].group.should.match(/.{20}/);
                 }
             }
+        });
+    });
+
+    describe('Create Group', function () {
+        var form;
+
+        before(function (done) {
+            form = pagePersonal.querySelector('#form-create');
+            window.setTimeout(done, 0);
+        });
+
+        it('should fire "iron-form-invalid" with empty inputs', function (done) {
+            var inputs = [
+                { name: 'paper-input[name=code]', value: '' },
+                { name: 'paper-input[name=title]', value: '' },
+                { name: 'paper-input[name=description]', value: '' }
+            ];
+            var button = 'paper-button#button-search';
+
+            listenToEventOnClickingButton(form, 'iron-form-invalid', inputs, button, done);
+        });
+
+        it('should fire "iron-form-submit" with any input', function (done) {
+            var inputs = [
+                { name: 'paper-input[name=code]', value: 'Not a Code' },
+                { name: 'paper-input[name=title]', value: 'The Title' },
+                { name: 'paper-input[name=description]', value: 'The Description' }
+            ];
+            var button = 'paper-button#button-search';
+
+            listenToEventOnClickingButton(form, 'iron-form-submit', inputs, button, done);
+        });
+
+        it('should show an error message with an invalid code', function (done) {
+            function assertions() {
+                var codeInput = form.querySelector('paper-input[name=code]');
+                codeInput.errorMessage.should.equal('Please enter a valid code.');
+                codeInput.invalid.should.equal(true);
+            }
+
+            var inputs = [
+                { name: 'paper-input[name=code]', value: 'WCTTEST' },
+                { name: 'paper-input[name=title]', value: 'The Title' },
+                { name: 'paper-input[name=description]', value: 'The Description' }
+            ];
+            var button = 'paper-button#button-search';
+
+            listenToEventOnClickingButton(form, 'iron-form-submit', inputs, button, done, assertions);
+        });
+
+        it('should show the created group in "Owned Groups"', function () {
+            var container = pagePersonal.querySelector('#container-groups-owned');
+            var children = container.childNodes;
+            var createdGroup = children[children.length - 2];
+            createdGroup.textContent.should.match(/WCTTEST\/The Title/);
+        });
+
+        it('should show an error message registering the same code again', function (done) {
+            function assertions() {
+                var codeInput = form.querySelector('paper-input[name=code]');
+                codeInput.errorMessage.should.equal('Code "WCTTEST" already exists.');
+                codeInput.invalid.should.equal(true);
+            }
+
+            var inputs = [
+                { name: 'paper-input[name=code]', value: 'WCTTEST' },
+                { name: 'paper-input[name=title]', value: 'The Title' },
+                { name: 'paper-input[name=description]', value: 'The Description' }
+            ];
+            var button = 'paper-button#button-search';
+
+            listenToEventOnClickingButton(form, 'iron-form-submit', inputs, button, done, assertions);
         });
     });
 
