bd60f682bb5b5a1596ac23c3ad1586c77c6ccf03
Kevin Kwan <KK1241@googlemail.com>
2015-09-30 00:34:58 +0100

    Add chapter.

--- browser/behaviors/chapter-behavior/chapter-behavior.js
+++ browser/behaviors/chapter-behavior/chapter-behavior.js
@@ -13,7 +13,35 @@
         return numbers.join('.');
     },
 
-    addChapter: function (key, title, indentation) { },
+    /**
+     * Add new chapter to Noootes before the target key.
+     *
+     * @param {String} group - Group ID.
+     * @param {String} key - Key to add group before.
+     * @param {String} title - Title of new chapter.
+     * @param {Number} indentation - Indentation of new chapter.
+     */
+    addChapter: function (group, key, title, indentation) {
+        var firebase = Noootes.FirebaseRef('notes/order/' + group);
+        var newChapterKey = firebase.push().key();
+
+        firebase.transaction(function (data) {
+            var newChapter = {
+                title: title,
+                indentation: indentation,
+                next: key,
+                previous: data[key].previous
+            };
+
+            data[newChapterKey] = newChapter;
+            // Attach new key to previous in chain.
+            data[data[key].previous].next = newChapterKey;
+            // Attach new key to next in chain.
+            data[key].previous = newChapterKey;
+
+            return data;
+        });
+    },
 
     editChapter: function (key, title, indentation) { },
 
