5c660e8f6246cc48b53d36efc08d1043971ce0ad
Kevin Kwan <KK1241@googlemail.com>
2015-10-01 19:59:00 +0100

    Copy page-editor tests to page-reader.

--- browser/test/pages/page-reader/page-reader-test.js
+++ browser/test/pages/page-reader/page-reader-test.js
@@ -1,10 +1,107 @@
 chai.should();
 
 describe('<page-reader>', function () {
-    it('should display a message if no group is selected');
-    it('should change user and group on hashchange');
-    it('should display a message if the user doesn\t exist');
-    it('should display a message if the group doesn\'t exist');
-    it('should display a message if the user isn\'t allowed to write to the group');
-    it('should remove messages if group is valid');
+    function listenOnce(eventName, assertions) {
+        function listener(event) {
+            window.removeEventListener(eventName, listener);
+            assertions(event);
+        }
+
+        window.addEventListener(eventName, listener);
+    }
+
+    var pageReader;
+    var readerError;
+
+    before(function () {
+        pageReader = document.querySelector('page-editor');
+        readerError = pageReader.querySelector('#reader-error');
+    });
+
+    it('should display a message if no group is selected', function () {
+        pageReader._selectedPage.should.equal(0);
+        readerError.textContent.should.equal('No group selected. Please select a group from your Noootes.');
+    });
+
+    it('should change user and group on hashchange', function (done) {
+        listenOnce('hashchange', function () {
+            pageReader.user.should.equal('user');
+            pageReader.code.should.equal('CODE');
+
+            done();
+        });
+
+        window.location.hash = '/reader/user/CODE';
+    });
+
+    it('should display a message if the user doesn\'t exist', function (done) {
+        listenOnce('hashchange', function () {
+            var handle = window.setInterval(function () {
+                if (readerError.textContent === 'Couldn\'t find a user with the name "FakeUser".') {
+                    window.clearInterval(handle);
+                    done();
+                }
+            }, 100);
+        });
+
+        window.location.hash = '/reader/FakeUser/CODE';
+    });
+
+    it('should display a message if the group doesn\'t exist', function (done) {
+        listenOnce('hashchange', function () {
+            var handle = window.setInterval(function () {
+                if (readerError.textContent === 'Couldn\'t find a group with the code "CODE".') {
+                    window.clearInterval(handle);
+                    done();
+                }
+            }, 100);
+        });
+
+        window.location.hash = '/reader/WCT/CODE';
+    });
+
+    it('should display a message if the user isn\'t allowed to read the group\'s notes', function (done) {
+        listenOnce('hashchange', function () {
+            var handle = window.setInterval(function () {
+                if (readerError.textContent === 'You don\'t have permission to read to these Noootes.') {
+                    window.clearInterval(handle);
+                    done();
+                }
+            }, 100);
+        });
+
+        window.location.hash = '/reader/Kek/WCTPRIVATE';
+    });
+
+    it('should remove messages if group is valid', function (done) {
+        listenOnce('hashchange', function () {
+            var handle = window.setInterval(function () {
+                if (readerError.textContent === '') {
+                    pageReader._selectedPage.should.equal(1);
+
+                    window.clearInterval(handle);
+                    done();
+                }
+            }, 100);
+        });
+
+        window.location.hash = '/reader/WCT/PERMANENT';
+    });
+
+    it('should set the hash to previous valid group on changing back to #/reader/', function (done) {
+        listenOnce('hashchange', function () {
+            var handle = window.setInterval(function () {
+                if (window.location.hash === '#/reader/WCT/PERMANENT') {
+                    window.clearInterval(handle);
+                    done();
+                }
+            }, 100);
+        });
+
+        window.location.hash = '/reader/';
+    });
+
+    describe('Noootes', function () {
+        it('has no tests written yet');
+    });
 });
