e03d2e7a1e66d2ec4704ce56983b2ceef97c498a
Kevin Kwan <KK1241@googlemail.com>
2015-10-01 19:38:33 +0100

    Add a random test for noootes-chapter.

--- browser/test/noootes-chapter/noootes-chapter-test.js
+++ browser/test/noootes-chapter/noootes-chapter-test.js
@@ -1,5 +1,22 @@
 chai.should();
 
 describe('<noootes-chapter>', function () {
-    it('should fire "chapter-select" on tapping body');
+    var noootesChapter;
+
+    before(function () {
+        noootesChapter = document.querySelector('noootes-chapter');
+    });
+
+    it('should set text content on chapter change', function () {
+        noootesChapter.chapter = {
+            next: 'end',
+            previous: 'start',
+            indentation: 1,
+            title: 'A Test Chapter'
+        };
+        noootesChapter.chapterNumber = '1.1';
+
+        noootesChapter.textContent.should.equal('1.1A Test Chapter');
+        noootesChapter.indentation.should.equal(1);
+    });
 });
